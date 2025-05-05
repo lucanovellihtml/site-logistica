177b0d35ccc57b0b5c27c7cfda564f58d6a844ec
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-05-05 09:50:00 +0200

    Aggiornato: aggiornata la logica di gestione del fullscreen, viene utilizzata la property del bottone "aria-expanded" per gestire la visualizzazione o non

--- js/header/scrip-fullscreen-menu.js
+++ js/header/scrip-fullscreen-menu.js
@@ -3,20 +3,32 @@
   Script che gestisce l'animazione del menu' a tendina quando clicco il nav button.
 */
 const openMenuBtn = document.getElementById('openMenuBtn');
-const closeMenuBtn = document.getElementById('closeMenuBtn');
 const fullscreenMenu = document.getElementById('navbarNavAltMarkup');
 
-// Quando clicco il bottone di open, mi viene mostrato il fullscreen menu con l'aggiunta della classe css 'open'
+/* Quando clicco il bottone di open, viene controllata la property che gestire la visualizzazione del
+  fullscreen menu con l'aggiunta della classe css 'open'.
+  Vengono richiamare le due funzioni activeMenu e notActiveMenu.
+*/
 openMenuBtn.addEventListener('click', () => {
+
+    console.log(openMenuBtn.getAttribute("aria-expanded"));
+    const flagOpenMenu = openMenuBtn.getAttribute("aria-expanded")
+    if (flagOpenMenu === "true")
+        activeMenu();
+    else
+        notActiveMenu();
+});
+
+function activeMenu() {
     fullscreenMenu.style.display = 'block';
     setTimeout(() => {
         fullscreenMenu.classList.add('open');
     }, 300);
-});
+}
 
-/*closeMenuBtn.addEventListener('click', () => {
+function notActiveMenu() {
     fullscreenMenu.classList.remove('open');
     setTimeout(() => {
         fullscreenMenu.style.display = 'none';
     }, 300); // Corrisponde alla durata della transizione (0.3s)
-});*/
+}
