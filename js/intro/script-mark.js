91c35e1836d3cbf0165a7a479505597060d79335
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-07-02 16:12:38 +0200

    Aggiornato: aggiunta la funzione per la gestione del titolo della pagina in inglese

--- js/intro/script-mark.js
+++ js/intro/script-mark.js
@@ -11,7 +11,23 @@ const markInstance = new Mark(title);
 // Titolo da sottolineare
 const titleMarked = "Benvenuto in BV Express Logistic";
 
+// Titolo da sottolineare in inglese
+const titleMarkedEn = "Welcome to BV Express Logistics";
+
+// Funzione per la pagina in italiano
 markInstance.mark(titleMarked, {
+    "separateWordSearch": false, // Disabilita la sottolineatura delle parole separate
+    "done": function () {
+        const elementiEvidenziati = title.querySelectorAll('mark');
+        elementiEvidenziati.forEach(elemento => {
+            // Aggiungi una classe per attivare l'animazione
+            elemento.classList.add('animate');
+        });
+    }
+});
+
+// Funzione per la pagina in inglese
+markInstance.mark(titleMarkedEn, {
     "separateWordSearch": false, // Disabilita la sottolineatura delle parole separate
     "done": function () {
         const elementiEvidenziati = title.querySelectorAll('mark');
