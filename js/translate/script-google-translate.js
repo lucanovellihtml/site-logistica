245613c99465528bff19e8dec9e5dfd9193793f8
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2025-07-02 16:11:27 +0200

    Eliminato perchè non è utilizzato

--- js/translate/script-google-translate.js
+++
@@ -1,8 +0,0 @@
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({
-        pageLanguage: 'it',
-        includedLanguages: 'en,it',
-        layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
-        autoDisplay: false
-    }, 'google_translate_element');
-}
