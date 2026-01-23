2fb0f54a23ca0ee164790b872d9476585321c3c0
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2026-01-23 11:14:09 +0100

    Aggiunto: aggiunto il riferimento alla velocità del movimento, allo start del movimento continuo senza bloccarsi all'interazione dell'utente

--- js/intro/script-swiper-section-service.js
+++ js/intro/script-swiper-section-service.js
@@ -1,7 +1,19 @@
 const swiperEl = document.getElementById("swiperProject");
+
 Object.assign(swiperEl, {
     loop: true,
     slidesPerView: "auto",
     spaceBetween: 30,
+    speed: 8000,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: false,
+    },
+    freeMode: {
+        enabled: true,
+        momentum: false,
+    },
 });
+
 swiperEl.initialize();
